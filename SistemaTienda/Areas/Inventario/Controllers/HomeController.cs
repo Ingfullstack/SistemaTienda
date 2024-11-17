c840cf1e891906baebc233ea2434b418e5b71e07
Carlos Ayala <ing.full.stack@hotmail.com>
2024-11-17 19:09:00 -0400

    Diseno Pagina Principal

--- SistemaTienda/Areas/Inventario/Controllers/HomeController.cs
+++ SistemaTienda/Areas/Inventario/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using SistemaTienda.AccesoDatos.Repositorio.IRepositorio;
+using SistemaTienda.Modelo.Models;
 using System.Diagnostics;
 
 namespace SistemaTienda.Areas.Inventario.Controllers
@@ -6,9 +8,18 @@ namespace SistemaTienda.Areas.Inventario.Controllers
     [Area("Inventario")]
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly IUnidadTrabajo unidadTrabajo;
+
+        public HomeController(IUnidadTrabajo unidadTrabajo)
         {
-            return View();
+            this.unidadTrabajo = unidadTrabajo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Producto> productoLista = await unidadTrabajo.Producto.ObtenerTodos();
+            return View(productoLista);
         }
 
         [HttpGet]
